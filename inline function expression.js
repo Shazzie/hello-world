5d1c9df2c2ef13fc486e383d8f14398602c8cb03
Sharon Halliday <shazzyhal@gmail.com>
2018-02-03 20:10:52 +0000

    commit

---
+++ inline function expression.js
@@ -0,0 +1,14 @@
+// Function expression that assigns the function displayFavorite
+// to the variable favoriteMovie
+var favoriteMovie = function displayFavorite(movieName) {
+  console.log("My favorite movie is " + movieName);
+};
+
+// Function declaration that has two parameters: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+  messageFunction(name);
+}
+
+// Call the movies function, pass in the favoriteMovie function and name of movie
+movies(favoriteMovie, "Finding Nemo");
