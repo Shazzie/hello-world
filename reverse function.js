3f9b131ea7ba057e06108fceb775998587268b6d
Sharon Halliday <shazzyhal@gmail.com>
2018-02-03 17:55:10 +0000

    reverse function

---
+++ reverse function.js
@@ -0,0 +1,8 @@
+function reverseString(reverse me) {var reversed=""
+for (var i= reverseMe.length -1; i>=0; i--) {
+
+reversed += reverseMe[i];
+}
+return reversed;
+}
+console.log(reverseString("Shazzie"))
