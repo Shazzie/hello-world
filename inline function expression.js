cf37f03a21ea2a8302e288d528f55339cc7eada8
Sharon Halliday <shazzyhal@gmail.com>
2018-02-03 20:11:55 +0000

    commit

--- inline function expression.js
+++ inline function expression.js
@@ -12,3 +12,16 @@ function movies(messageFunction, name) {
 
 // Call the movies function, pass in the favoriteMovie function and name of movie
 movies(favoriteMovie, "Finding Nemo");
+
+bypasing;
+
+// Function declaration that takes in two arguments: a function for displaying
+// a message, along with a name of a movie
+function movies(messageFunction, name) {
+  messageFunction(name);
+}
+
+// Call the movies function, pass in the function and name of movie
+movies(function displayFavorite(movieName) {
+  console.log("My favorite movie is " + movieName);
+}, "Finding Nemo");
